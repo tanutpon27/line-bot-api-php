f155dd846f28230b8f25d6c3ef8e2849e295e965
tanutpon27 <56038263+tanutpon27@users.noreply.github.com>
2020-03-11 19:49:39 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'RyVe1lB2c33yly9iqmfVgCssiIkHDMQuztjgsHzSfX8pokfjhUO5XUeba3t+C7P8XyejdPl/vaZHXsNMBphw9V99XxsMAbONLPtu5myLUpbLX0CxIqeg6P+CAHmqXvhekBUjoeUUNOF6OkxYmqSnKwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
